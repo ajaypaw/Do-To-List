31c7df7fb47c6a8939b1325a77f1a107f0eab48c
Ajay Pawar <160529285+ajaypaw@users.noreply.github.com>
2024-10-09 12:33:01 +0000

    Pending changes exported from your codespace

--- scrip.js
+++ scrip.js
@@ -1,23 +1,31 @@
 const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
 
-let j = 0 ;
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+//let j = 0 ;
+// function getRandomColor() {
+//     const letters = '0123456789ABCDEF';
+//     let color = '#';
+//     for (let i = 0; i < 6; i++) {
+//         color += letters[Math.floor(Math.random() * 16)];
+//     }
+//     return color;
+// }
+
+// function changeColor() {
+//     const randomColor = getRandomColor();
+//     li =document.getElementsByTagName('li')[j];
+//     li.style.colo = randomColor;
+//     j = j+1;
+// }
+const button =document.getElementById("button");
+button.addEventListener('click',function(){
+    addTask();
+});
+inputBox.addEventListener("keypress",function(event){
+    if(event.key === "Enter"){
+        addTask();
     }
-    return color;
-}
-
-function changeColor() {
-    const randomColor = getRandomColor();
-    li =document.getElementsByTagName('li')[j];
-    li.style.colo = randomColor;
-    j = j+1;
-}
-
+});
 
 function addTask(){
     if(inputBox.value===""){
